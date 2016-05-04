d78cadd7c114c920b2d6876cd0ccb9979c36dc2e
yessi0212 <yesi.perez.0599@gmail.com>
2016-05-04 07:09:33 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,14 @@
 // JavaScript Document
 
-$(Document).ready(function(e) {
+$(document).ready(function (e){
     document.addEventListener("deviceready",function(){
 		
-		$('#beep').tap(function () {
+		$('#beep').tap(function() {
 			navigator.notification.beep (1);
-		})//tap beep
+		});//tap beep
 		
 		$('#vibrar').tap(function(){
 			navigator.notification.vibrate (1000);
-		})//tap vibrar
+		});//tap vibrar
 	}, false);//deviceready
 });//ready
